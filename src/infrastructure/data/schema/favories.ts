92157013b83b4c1c2cbf991be53f5bdd4ce5c370
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-05-16 15:44:00 +0200

    Correct favories.ts

--- src/infrastructure/data/schema/favories.ts
+++ src/infrastructure/data/schema/favories.ts
@@ -2,7 +2,7 @@ import { pgTable, uuid, timestamp } from 'drizzle-orm/pg-core';
 import { users } from './';
 import { posts } from './';
 
-export const favorites = pgTable('favorites', {
+export const favories = pgTable('favorites', {
     id: uuid('id').defaultRandom().primaryKey(),
     userId: uuid('user_id').references(() => users.id).notNull(),
     postId: uuid('post_id').references(() => posts.id).notNull(),
